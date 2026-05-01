5a3f66b8dee77798009a7f0d9c427a3982fe9456
Natália Amaral <natalia.amaraldoval@gmail.com>
2026-05-01 17:19:19 -0300

    Implementa as classes concretas de relatórios

--- SistemaRelatorios.java
+++ SistemaRelatorios.java
@@ -7,3 +7,24 @@ interface IVisitorExportacao {
 interface IRelatorio {
     void accept(IVisitorExportacao visitor);
 }
+
+class RelatorioTexto implements IRelatorio {
+    public String conteudo = "Texto do relatorio";
+    public void accept(IVisitorExportacao visitor) {
+        visitor.visitRelatorioTexto(this);
+    }
+}
+
+class RelatorioImagem implements IRelatorio {
+    public String nomeImagem = "foto.png";
+    public void accept(IVisitorExportacao visitor) {
+        visitor.visitRelatorioImagem(this);
+    }
+}
+
+class RelatorioGrafico implements IRelatorio {
+    public String dadosGrafico = "[100, 200, 500]";
+    public void accept(IVisitorExportacao visitor) {
+        visitor.visitRelatorioGrafico(this);
+    }
+}
